6ec60cbb9f7b4eea08a654749bc18cb31195efe7
Ford Atwater <fordatwater@gmail.com>
2019-12-02 18:01:51 -0800

    ccapture working possibly

--- donut/sketch.js
+++ donut/sketch.js
@@ -1,22 +1,32 @@
 
-function setup() {
-  createCanvas(400, 400, WEBGL);
-}
 
-function draw() {
+// // the frame rate (frames per second)
+// var fps = 30;
+
+// // the canvas capturer instance
+// var capturer = new CCapture({ format: 'png', framerate: fps });
+
+// function setup() {
+
+//   createCanvas(400, 400, WEBGL);
+
+//  // image(space,10,10,50,50)
+
+
+// }
+
+// function draw() {
   
-//  ambientLight(0,0,180);
-//  directionalLight(255,100,100,10);
-  
-  
-  background(0);
-  normalMaterial();
- // ambientMaterial(250);
-  noStroke();
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  torus(70, 20);
-  pop();
-}
+//  // ambientLight(0,0,180);
+// //  directionalLight(255,100,100,10);
+//   background(0);
+
+//   normalMaterial();
+//   // //  ambientMaterial(250);
+//   noStroke();
+//   rotateZ(frameCount * 0.003);
+//   rotateX(frameCount * -0.01);
+//   rotateY(frameCount * 0.01);
+//   torus(90, 25);
+
+// }
