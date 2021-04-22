dff459bd1b0b98f699b2fca680f82709daeb381a
fordat <fordatwater@gmail.com>
2021-04-22 14:28:05 -0700

    sort prac

--- javascript-practice/algorithms.js
+++ javascript-practice/algorithms.js
@@ -1,1 +1,28 @@
-console.log("you're in an algorithm bro!")
+let vals = [5, 4, 9, 2, 1];
+let letters = ["a", "B", "hello", "Aardvark"]
+let mixed = ["a", 2, 420, "Big"]
+
+let pairs = [{
+    x: 2,
+    y: 10
+}, {
+    x: 5,
+    y: 10000
+}, {
+    x: 100,
+    y: 100
+}
+];
+
+function compare(a,b) {
+    return a.y - b.y
+}
+
+
+let s = "It was a dark and stormy night."
+let words = s.split(/\W+/).filter(word => word.length >= 2)
+words.sort((a,b) => a.length - b.length)
+
+console.log(words);
+pairs.sort(compare);
+console.log(pairs);
