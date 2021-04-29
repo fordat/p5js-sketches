dd530f8afe7a3121f3be8e0c9784b5f0702ec329
fordat <fordatwater@gmail.com>
2021-04-29 12:21:23 -0700

    update

---
+++ javascript-practice/variables.js
@@ -0,0 +1,13 @@
+function setup() {
+
+    for (let i = 0; i < 100; i++) {
+        //stuff
+        if (mouseX > 20) {
+            let j = 20
+        }
+        console.log(j);
+    }
+
+
+    console.log(i);
+}
