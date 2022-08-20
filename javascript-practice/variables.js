be0dffbeeef6f513575c33ead3a5b68e4a7491d7
fordat <fordatwater@gmail.com>
2022-08-20 11:30:10 -0700

    variables update

--- javascript-practice/variables.js
+++ javascript-practice/variables.js
@@ -5,7 +5,7 @@ let vals = [3, 6, 31, 6, 2]
 
 let bingo = vals.reduce((acc, val) => 
     val > acc ? val : acc 
-);
+, 50);
 console.log(bingo);
 
 // let answer = vals.reduce((acc,val) => acc + val, 10);
